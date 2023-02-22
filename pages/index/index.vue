7bbe5d336a9c409a218ca63a8842650d9008dbd4
pycoder1001 <2327296693@qq.com>
2023-02-22 09:47:01 +0800

    ax

---
+++ pages/index/index.vue
@@ -0,0 +1,53 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/201903130940468171843.png" mode="widthFix"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'testDeom'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		
+		width: 400rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+		
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
